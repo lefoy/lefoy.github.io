d48335b84e1397d1dcc2c45ff0e2d3f370a50ed2
lefoy <lefoy@lefoy.net>
2014-08-26 00:17:45 -0400

    Update theme (home section height)

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+    $('.section.home').height( $(window).height() );
+    $(window).on('resize', function(event) {
+        $('.section.home').height( $(window).height() );
+    });
+
     $('.section.stats').bind('inview', function(event, isInView) {
         if ( isInView ) {
             $('.odometer').each(function() {
