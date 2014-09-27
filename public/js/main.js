cb6b276d92f2dc485bcad22debdb75e40cbc600a
lefoy <lefoy@lefoy.net>
2014-09-27 01:12:45 -0400

    Update js stats

--- public/js/main.js
+++ public/js/main.js
@@ -27,8 +27,8 @@ $(document).ready(function() {
     });
 
     function twitterCallback(data) {
-        $('.odometer.tweets').attr('data-number', data.results[1].text);
-        $('.odometer.followers').attr('data-number', data.results[7].text);
+        $('.odometer.tweets').attr('data-number', data.results.collection1[1].text);
+        $('.odometer.followers').attr('data-number', data.results.collection1[7].text);
     }
 
     $.ajax({
