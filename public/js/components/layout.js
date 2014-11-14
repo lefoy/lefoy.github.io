604c41639b1e272d1835fd9780dcb76a8b937a67
lefoy <me@lefoy.net>
2014-11-14 10:08:16 -0500

    Update particles config

--- public/js/components/layout.js
+++ public/js/components/layout.js
@@ -74,40 +74,21 @@ var layout = (function(window, document, $) {
 
         particles = function() {
             particlesJS('particles', {
+                canvas: {
+                    color_hex_bg: '#000',
+                    opacity: 1
+                },
                 particles: {
-                    color: '#fff',
-                    shape: 'circle', // "circle", "edge" or "triangle"
-                    opacity: 0.5,
-                    size: 2,
+                    color_hex: '#fff',
+                    opacity: 1,
+                    size: 2.5,
                     size_random: true,
                     nb: 100,
-                    line_linked: {
-                        enable_auto: true,
-                        distance: 250,
-                        color: '#fff',
-                        opacity: 0.5,
-                        width: 1,
-                        condensed_mode: {
-                            enable: true,
-                            rotateX: 600,
-                            rotateY: 600
-                        }
-                    },
                     anim: {
-                        enable: true,
                         speed: 2
                     }
                 },
-                interactivity: {
-                    enable: true,
-                    mouse: {
-                        distance: 250
-                    },
-                    detect_on: 'canvas', // "canvas" or "window"
-                    mode: 'grab'
-                },
-                /* Retina Display Support */
-                retina_detect: false
+                retina_detect: true
             });
         },
 
