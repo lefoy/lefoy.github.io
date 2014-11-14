e709c4aefaf4bd561c3b5af29692a4c5382b3455
lefoy <me@lefoy.net>
2014-11-14 10:10:10 -0500

    Update particles config

--- public/js/components/layout.js
+++ public/js/components/layout.js
@@ -83,7 +83,7 @@ var layout = (function(window, document, $) {
                     opacity: 1,
                     size: 2.5,
                     size_random: true,
-                    nb: 140,
+                    nb: 120,
                     anim: {
                         speed: 2
                     }
