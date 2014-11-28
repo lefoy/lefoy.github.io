0ef698e14081043c55f024448499f863b77322f6
lefoy <me@lefoy.net>
2014-11-27 21:12:02 -0500

    Terminal test

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,41 @@ console.log(
 $(window).konami({
     cheat: function() {
         alert('Cheat code activated!');
+
+        $('body').append('<div id="terminal"></div>');
+
+        var terminal = $('body').find('#terminal');
+
+        terminal.css({
+            position: 'fixed',
+            top: '0',
+            left: '0',
+            width: '100%',
+            height: '100%',
+            color: '#fff',
+            zIndex: '9999',
+            backgroundColor: '#000'
+        });
+
+        $('#terminal').terminal(function(command, term) {
+            if (command !== '') {
+                try {
+                    var result = window.eval(command);
+                    if (result !== undefined) {
+                        term.echo(new String(result));
+                    }
+                } catch (e) {
+                    term.error(new String(e));
+                }
+            } else {
+                term.echo('');
+            }
+        }, {
+            greetings: 'Javascript Interpreter',
+            name: 'js_demo',
+            height: 200,
+            prompt: 'js> '
+        });
     }
 });
 
