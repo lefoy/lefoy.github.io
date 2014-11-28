3cfaa1004c3fef94147f2556550fdbeaef81dc71
lefoy <me@lefoy.net>
2014-11-27 21:49:35 -0500

    Terminal tests

--- public/js/main.js
+++ public/js/main.js
@@ -3,57 +3,55 @@ console.log(
     'background: #000; color: #fff; padding: 10px 3px; line-height: 70px;'
 );
 
-$(window).konami({
-    cheat: function() {
-        $('body').append('<div id="terminal"></div>');
+function terminal() {
+    $('body').append('<div id="terminal"></div>');
 
-        var terminal = $('body').find('#terminal');
+    var terminal = $('body').find('#terminal');
 
-        $('#terminal').terminal(function(command, term) {
-            switch (command) {
-                case 'help':
-                    term.echo(['',
-                        '    The most commonly used commands are:',
-                        '        github         Open the website Github repository',
-                        '        summary        Display information about the website',
-                        '        contact        Send email to website author',
-                        ''
-                    ].join('\n'));
-                    break;
-                case 'github':
-                    term.echo('github command...\n');
-                    break;
-                case 'summary':
-                    term.echo('summary command...\n');
-                    break;
-                case 'contact':
-                    term.echo('contact command...\n');
-                    break;
-                case 'exit':
-                    $('#terminal').remove();
-                    break;
-                default:
-                    term.echo('command not found: ' + command + '\n');
-            }
-        }, {
-            greetings: '',
-            name: 'terminal',
-            prompt: '> ',
-            exit: false
-        });
+    $('#terminal').terminal(function(command, term) {
+        switch (command) {
+            case 'help':
+                term.echo(['',
+                    '    The most commonly used commands are:',
+                    '        github         Open the website Github repository',
+                    '        summary        Display information about the website',
+                    '        contact        Send email to website author',
+                    ''
+                ].join('\n'));
+                break;
+            case 'github':
+                term.echo('github command...\n');
+                break;
+            case 'summary':
+                term.echo('summary command...\n');
+                break;
+            case 'contact':
+                term.echo('contact command...\n');
+                break;
+            case 'exit':
+                $('#terminal').remove();
+                break;
+            default:
+                term.echo('command not found: ' + command + '\n');
+        }
+    }, {
+        greetings: '',
+        name: 'terminal',
+        prompt: '> ',
+        exit: false
+    });
 
-        terminal.css({
-            position: 'fixed',
-            top: '0',
-            left: '0',
-            width: '100%',
-            height: '100%',
-            color: '#fff',
-            zIndex: '1000000',
-            backgroundColor: 'rgba(0, 0, 0, 0.9)'
-        });
-    }
-});
+    terminal.css({
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        width: '100%',
+        height: '100%',
+        color: '#fff',
+        zIndex: '1000000',
+        backgroundColor: 'rgba(0, 0, 0, 0.9)'
+    });
+}
 
 jQuery(document).ready(function($) {
 
