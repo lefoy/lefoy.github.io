50d00789a94da5ed96affbbe34ff11f317f088a9
lefoy <me@lefoy.net>
2014-11-27 22:42:09 -0500

    Add terminal easter egg

---
+++ public/js/components/terminal.js
@@ -0,0 +1,88 @@
+var terminal = (function(window, document, $) {
+
+    'use strict';
+
+    var terminal,
+
+        init = function() {
+
+            bindEvents();
+
+        },
+
+        bindEvents = function() {
+
+            $(window).konami({
+                cheat: function() {
+                    terminalInit();
+                }
+            });
+
+        },
+
+        layout = function() {
+
+            terminal.css({
+                position: 'fixed',
+                top: '0',
+                left: '0',
+                width: '100%',
+                height: '100%',
+                color: '#fff',
+                zIndex: '1000000',
+                backgroundColor: 'rgba(0, 0, 0, 0.9)'
+            });
+        },
+
+        terminalInit = function() {
+
+            $('body').append('<div id="terminal"></div>');
+
+            terminal = $('body').find('#terminal');
+
+            $('#terminal').terminal(function(command, term) {
+                switch (command) {
+                    case 'help':
+                        term.echo(['',
+                            '    list of available commands:',
+                            '        clear          Clean terminal output',
+                            '        github         Open the website Github repository',
+                            '        contact        Send email to website author',
+                            ''
+                        ].join('\n'));
+                        break;
+                    case 'github':
+                        window.location.href = 'https://github.com/lefoy/lefoy.github.io';
+                        break;
+                    case 'contact':
+                        window.location.href = "mailto:me@lefoy.net";
+                        break;
+                    case 'rm -rf /':
+                    case 'rm -fr /':
+                        term.echo('\n');
+                        $('.terminal-output').append('<img src="/public/img/terminal/rm-rf.jpg" alt="" />');
+                        term.echo('\n');
+                        break;
+                    case 'exit':
+                        $('#terminal').remove();
+                        break;
+                    default:
+                        term.echo('command not found: ' + command + '\n');
+                        term.echo('type \'help\' for a list of available commands\n');
+                }
+            }, {
+                greetings: 'type \'help\' for a list of available commands\n',
+                name: 'terminal',
+                prompt: '> ',
+                exit: false
+            });
+
+            layout();
+
+        };
+
+    return {
+        init: init
+    };
+
+})(window, document, $);
--- public/js/main.js
+++ public/js/main.js
@@ -1,60 +1,20 @@
-console.log(
-    '%c lefoy.net - Have fun hacking!   |   Tip: try the Konami code ',
-    'background: #000; color: #fff; padding: 10px 3px; line-height: 70px;'
-);
-
-function terminal() {
-    $('body').append('<div id="terminal"></div>');
-
-    var terminal = $('body').find('#terminal');
-
-    $('#terminal').terminal(function(command, term) {
-        switch (command) {
-            case 'help':
-                term.echo(['',
-                    '    The most commonly used commands are:',
-                    '        github         Open the website Github repository',
-                    '        summary        Display information about the website',
-                    '        contact        Send email to website author',
-                    ''
-                ].join('\n'));
-                break;
-            case 'github':
-                term.echo('github command...\n');
-                break;
-            case 'summary':
-                term.echo('summary command...\n');
-                break;
-            case 'contact':
-                term.echo('contact command...\n');
-                break;
-            case 'exit':
-                $('#terminal').remove();
-                break;
-            default:
-                term.echo('command not found: ' + command + '\n');
-        }
-    }, {
-        greetings: '',
-        name: 'terminal',
-        prompt: '> ',
-        exit: false
-    });
-
-    terminal.css({
-        position: 'fixed',
-        top: '0',
-        left: '0',
-        width: '100%',
-        height: '100%',
-        color: '#fff',
-        zIndex: '1000000',
-        backgroundColor: 'rgba(0, 0, 0, 0.9)'
-    });
+function banner() {
+    console.log(
+        '%c lefoy.net   |   Tip: try the Konami code ',
+        'background: #000; color: #fff; padding: 10px 3px; line-height: 35px;'
+    );
+    console.log(
+        '%c --------- ↑ ↑ ↓ ↓ ← → ← → B A  --------- ',
+        'background: #000; color: #fff; padding: 10px 3px; line-height: 35px;'
+    );
+    console.log('');
 }
 
 jQuery(document).ready(function($) {
 
+    banner();
+
     layout.init();
+    terminal.init();
 
 });
